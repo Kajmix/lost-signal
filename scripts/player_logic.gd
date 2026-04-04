06590c3f2a72007f1ef993a4df3a44a6be5e7cff
Kajmix <kacpermizgala1@gmail.com>
2026-04-04 14:25:18 +0100

    new textures, got rid of screen shaking

--- scripts/player_logic.gd
+++ scripts/player_logic.gd
@@ -1,8 +1,21 @@
 extends CharacterBody2D
 
+@onready var player_sprite : AnimatedSprite2D = $AnimatedSprite2D
+
 @export var player_speed = 100
 
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
+	var animation = "down"
 	var dir = Input.get_vector("left", "right", "up", "down")
+	if Input.is_action_pressed("left"):
+		animation = "left"
+	if Input.is_action_pressed("right"):
+		animation = "right"
+	if Input.is_action_pressed("up"):
+		animation = "up"
+	if Input.is_action_pressed("down"):
+		animation = "down"
+	if player_sprite.animation != animation:
+		player_sprite.animation = animation
 	velocity = dir * player_speed
 	move_and_slide()
